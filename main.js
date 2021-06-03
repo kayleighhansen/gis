c0420ab021edc833bcdfe96f7596dbc704df840d
Kayleigh Hansen <71479913+kayleighhansen@users.noreply.github.com>
2021-06-03 00:28:17 -0600

    it tracks me

--- main.js
+++ main.js
@@ -1,16 +1,35 @@
-require(["esri/config","esri/Map", "esri/views/MapView"], function (esriConfig,Map, MapView) {
+require(
+    [
+        "esri/config",
+        "esri/Map", 
+        "esri/views/MapView",
+        "esri/widgets/Locate",
+    ], 
     
-    esriConfig.apiKey = "AAPK72dc730c2917464995e9e10371db321aC2zo3yfnMZOCkiVW12V-auIzcHUgITJkFEN4Pd5eIWb0xvbxKBg5YaqPSA351CGk";
+    function (esriConfig, Map, MapView, Locate,) {
+    
+        esriConfig.apiKey = "AAPK72dc730c2917464995e9e10371db321aC2zo3yfnMZOCkiVW12V-auIzcHUgITJkFEN4Pd5eIWb0xvbxKBg5YaqPSA351CGk";
 
-    const map = new Map({
-      basemap: "arcgis-topographic" // Basemap layer service
-    });
+        const map = new Map({
+            basemap: "arcgis-topographic" // Basemap layer service
+        });
 
-    const view = new MapView({
-      map: map,
-      center: [-118.805, 34.027], // Longitude, latitude
-      zoom: 13, // Zoom level
-      container: "viewDiv" // Div element
-    });
+        const view = new MapView({
+            container: "viewDiv", // Div element
+            map: map,
+            center: [-40, 28],
+            zoom: 2
+        });
+
+        const locate = new Locate({
+            view: view,
+            useHeadingEnabled: false,
+            goToOverride: function(view, options) {
+                options.target.scale = 1500;
+                return view.goTo(options.target);
+            }
+        });
+          
+        view.ui.add(locate, "top-left");
 
   });
